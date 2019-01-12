f215eb7870c93213e4b4afe5ff81f0f25a4b91ef
Lidia Micusan <35565172+MicusanL@users.noreply.github.com>
2019-01-12 16:36:22 +0200

    First commit

--- src/Prediction.java
+++ src/Prediction.java
@@ -37,17 +37,10 @@
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.*;
 
 public class Prediction extends Component {
 
